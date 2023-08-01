5288ef688f687401ef79c4d0416c9f2c1679e3eb
Daniel Brink <brink.daniel@gmail.com>
2023-08-01 21:44:28 +1000

    Update AddMenuItem.Designer.cs

--- AddMenuItem.Designer.cs
+++ AddMenuItem.Designer.cs
@@ -123,6 +123,8 @@
 			// 
 			// textPath
 			// 
+			this.textPath.AcceptsReturn = true;
+			this.textPath.AcceptsTab = true;
 			this.textPath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
