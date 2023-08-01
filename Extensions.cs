5d4984515ce398e50d33fc7ad3515f5744181e74
Daniel Brink <brink.daniel@gmail.com>
2023-08-01 22:13:15 +1000

    Remove obsolete code

--- Extensions.cs
+++ Extensions.cs
@@ -29,14 +29,6 @@ namespace SSMSObjectExplorerMenu
 			}
 		}
 
-		public static string GetStringValue(this Enum value)
-		{
-			Type type = value.GetType();
-			FieldInfo fieldInfo = type.GetField(value.ToString());
-			StringValueAttribute[] attribs = fieldInfo.GetCustomAttributes(typeof(StringValueAttribute), false) as StringValueAttribute[];
-			return attribs.Length > 0 ? attribs[0].StringValue : null;
-		}
-
 
 		public static NodeInfo GetInfo(this INodeInformation node)
 		{
